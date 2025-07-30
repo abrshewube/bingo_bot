245be4d67d94b39fd1bcd8e9713b2e051c1d574e
abrshewube <abrshefru1427@gmail.com>
2025-07-30 00:45:03 -0700

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+       borderColor: {
+        border: '#E5E7EB',
+      },
+    },
   },
   plugins: [],
 };
